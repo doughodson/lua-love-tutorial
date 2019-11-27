56b78686fbd8a1d79435eae65920def042c040c3
Doug Hodson <doug@sidechannel.net>
2019-11-27 14:35:55 -0500

    update

--- game-02-zombie/main.lua
+++ game-02-zombie/main.lua
@@ -12,6 +12,7 @@ function love.load()
   player.speed = 180
 
   zombies = {}
+  bullets = {}
 end
 
 function love.update(dt)
@@ -28,23 +29,76 @@ function love.update(dt)
     player.x = player.x + player.speed * dt
   end
 
-  for i,z in ipairs(zombies) do
+  for i, z in ipairs(zombies) do
     z.x = z.x + math.cos(zombie_player_angle(z)) * z.speed * dt
     z.y = z.y + math.sin(zombie_player_angle(z)) * z.speed * dt
+
+    if distance_between(z.x, z.y, player.x, player.y) < 30 then
+      for i, z in ipairs(zombies) do
+        zombies[i] = nil
+      end
+    end
+  end
+
+  -- move bullets
+  for i, b in ipairs(bullets) do
+    b.x = b.x + math.cos(b.direction) * b.speed * dt
+    b.y = b.y + math.sin(b.direction) * b.speed * dt
+  end
+
+  -- remove bullets off screen
+  for i=#bullets, 1, -1 do
+    local b = bullets[i]
+    if b.x < 0 or b.y < 0 or b.x > love.graphics.getWidth() or b.y > love.graphics.getHeight() then
+      table.remove(bullets, i)
+    end
   end
 
 end
 
 function love.draw()
   love.graphics.draw(sprites.background, 0, 0)
+  -- draw player
   love.graphics.draw(sprites.player, player.x, player.y, player_mouse_angle(),
                      nil, nil,                          -- scale factor
                      sprites.player:getWidth()/2, sprites.player:getHeight()/2)
 
+  -- draw zombies
   for i,z in ipairs(zombies) do
     love.graphics.draw(sprites.zombie, z.x, z.y, zombie_player_angle(z),
                        nil, nil,
                        sprites.zombie:getWidth()/2, sprites.zombie:getHeight()/2)
+  end
+
+  -- draw bullets
+  for i,b in ipairs(bullets) do
+    love.graphics.draw(sprites.bullet, b.x, b.y, nil,
+                       0.5, 0.5,
+                       sprites.bullet:getWidth()/2, sprites.bullet:getHeight()/2)
+  end
+
+  for i, z in ipairs(zombies) do
+    for j, b in ipairs(bullets) do
+      if distance_between(z.x, z.y, b.x, b.y) < 20 then
+        z.dead = true
+        b.dead = true
+      end
+    end
+  end
+
+
+  for i=#zombies, 1, -1 do
+    local z = zombies[i]
+    if z.dead == true then
+      table.remove(zombies, i)
+    end
+  end
+
+  for i=#bullets, 1, -1 do
+    local b = bullets[i]
+    if b.dead == true then
+      table.remove(bullets, i)
+    end
   end
 
 end
@@ -61,13 +115,35 @@ function spawn_zombie()
   zombie = {}
   zombie.x = math.random(0, love.graphics.getWidth())
   zombie.y = math.random(0, love.graphics.getHeight())
-  zombie.speed = 100
+  zombie.speed = 140
+  zombie.dead = false
 
   table.insert(zombies, zombie)
+end
+
+function spawn_bullet()
+  bullet = {}
+  bullet.x = player.x
+  bullet.y = player.y
+  bullet.speed = 500
+  bullet.direction = player_mouse_angle()
+  bullet.dead = false
+
+  table.insert(bullets, bullet)
 end
 
 function love.keypressed(key, scancode, isrepeat)
   if key == "space" then
     spawn_zombie()
   end
+end
+
+function love.mousepressed(x, y, btn, isTouch)
+  if btn == 1 then
+    spawn_bullet()
+  end
+end
+
+function distance_between(x1, y1, x2, y2)
+  return math.sqrt((y2-y1)^2 + (x2-x1)^2)
 end
