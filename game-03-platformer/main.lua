3f5bc892a29ff104b5e9da952c663481e0c094bc
Doug Hodson <doug@sidechannel.net>
2019-11-28 21:44:03 -0500

    update

--- game-03-platformer/main.lua
+++ game-03-platformer/main.lua
@@ -1,5 +1,8 @@
 
 function love.load()
+  myWorld = love.physics.newWorld(0, 500)
+  myWorld:setCallbacks(beginContact, endContact, preSolve, postSolve)
+
   sprites = {}
   sprites.coin_sheet = love.graphics.newImage("sprites/coin_sheet.png")
   sprites.player_jump = love.graphics.newImage("sprites/player_jump.png")
@@ -7,12 +10,45 @@ function love.load()
 
   require("player")
 
+  platforms = {}
+  spawn_platform(50, 400, 300, 30)
 end
 
 function love.update(dt)
-
+  myWorld:update(dt)
+  playerUpdate(dt)
 end
 
 function love.draw()
-  love.graphics.draw(sprites.player_stand, player.x, player.y)
+  love.graphics.draw(sprites.player_stand, player.body:getX(), player.body:getY(), nil, nil, nil,
+                     sprites.player_stand:getWidth()/2, sprites.player_stand:getHeight()/2)
+
+  for i,p in ipairs(platforms) do
+    love.graphics.rectangle("fill", p.body:getX(), p.body:getY(), p.width, p.height)
+  end
+end
+
+function love.keypressed(key, scancode, isrepeat)
+  if key == "up" and player.grounded == true then
+    player.body:applyLinearImpulse(0, -2500)
+  end
+end
+
+function spawn_platform(x, y, width, height)
+  local platform = {}
+  platform.body = love.physics.newBody(myWorld, x, y, "static")
+  platform.shape = love.physics.newRectangleShape(width/2, height/2, width, height)
+  platform.fixture = love.physics.newFixture(platform.body, platform.shape)
+  platform.width = width
+  platform.height = height
+
+  table.insert(platforms, platform)
+end
+
+function beginContact(a, b, coll)
+  player.grounded = true
+end
+
+function endContact(a, b, coll)
+  player.grounded = false
 end
--- game-03-platformer/player.lua
+++ game-03-platformer/player.lua
@@ -1,4 +1,18 @@
 
 player = {}
-player.x = 100
-player.y = 100
+player.body = love.physics.newBody(myWorld, 100, 100, "dynamic")
+player.shape = love.physics.newRectangleShape(66, 92)
+player.fixture = love.physics.newFixture(player.body, player.shape)
+player.speed = 200
+player.grounded = false
+
+function playerUpdate(dt)
+  if love.keyboard.isDown("left") then
+    player.body:setX(player.body:getX() - player.speed * dt)
+  end
+
+  if love.keyboard.isDown("right") then
+    player.body:setX(player.body:getX() + player.speed * dt)
+  end
+
+end
